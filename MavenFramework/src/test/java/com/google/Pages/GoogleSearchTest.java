64cec7c2a108778f34e24fdb59a0f3a93676d488
debraj-dinda <debraj.dinda@gmail.com>
2020-03-06 13:57:17 +0530

    my first ncommit to local

---
+++ MavenFramework/src/test/java/com/google/Pages/GoogleSearchTest.java
@@ -0,0 +1,15 @@
+package com.google.Pages;
+
+import org.testng.Reporter;
+
+public class GoogleSearchTest {
+	
+	public void GoogleSearch() {
+	
+	Reporter.log("Debraj Dinda", true);
+	}
+
+}
+
+
+
